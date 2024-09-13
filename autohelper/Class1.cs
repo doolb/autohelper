e76e3ab96b611c3b5e790465853c5e77d07bfd50
xue-blood <xue.blood@outlook.com>
2024-09-13 16:00:31 +0800

    restruct

--- autohelper/Class1.cs
+++ autohelper/Class1.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using OpenCvSharp.Extensions;
+using OpenCvSharp;
+using System;
+using System.Diagnostics;
+using System.Drawing.Imaging;
 using System.Runtime.InteropServices;
 
 public class MouseOperations
@@ -68,6 +72,106 @@ public class MouseOperations
         {
             X = x;
             Y = y;
+        }
+    }
+}
+
+namespace autohelper
+{
+    internal partial class Program
+    {
+        public static Mat makeScreenshot()
+        {
+            if (adb)
+                return makeScreenshotAdb();
+            else
+                return BitmapConverter.ToMat(makeScreenshotbmp());
+        }
+
+        public static Bitmap makeScreenshotbmp()
+        {
+            Bitmap screenshot = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height, PixelFormat.Format32bppArgb);
+
+            Graphics gfxScreenshot = Graphics.FromImage(screenshot);
+
+            gfxScreenshot.CopyFromScreen(Screen.PrimaryScreen.Bounds.X, Screen.PrimaryScreen.Bounds.Y, 0, 0, Screen.PrimaryScreen.Bounds.Size, CopyPixelOperation.SourceCopy);
+
+            gfxScreenshot.Dispose();
+
+            return screenshot;
+        }
+
+        public static bool adb = false;
+        private static string adbport = "";
+
+        private static void initAdb(bool _screenshotadb)
+        {
+            adb = true;
+        }
+
+        public static Mat makeScreenshotAdb()
+        {
+            Process pr2 = new Process();
+            pr2.StartInfo.FileName = @"c:\windows\system32\cmd.exe";
+            pr2.StartInfo.Arguments = $"/c \"adb.exe {adbport}exec-out screencap -p > adb.png\"";
+            pr2.Start();
+            pr2.WaitForExit();
+
+            return Cv2.ImRead("adb.png");
+
+            var outputStream = new StreamWriter("adb.png");
+            Process process = new Process();
+            process.StartInfo.FileName = "adb.exe";
+            process.StartInfo.Arguments = "exec-out screencap -p";
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.OutputDataReceived += new DataReceivedEventHandler((sender, e) =>
+            {
+                if (!String.IsNullOrEmpty(e.Data))
+                {
+                    outputStream.WriteLine(e.Data);
+                }
+            });
+
+            process.Start();
+
+            process.BeginOutputReadLine();
+
+            process.WaitForExit();
+            process.Close();
+
+            outputStream.Close();
+
+            return Cv2.ImRead("adb.png");
+        }
+
+        private static int adb_x, adb_y;
+        public static void SetCursorPosition(int x, int y)
+        {
+            if (adb)
+            {
+                adb_x = x;
+                adb_y = y;
+            }
+            else
+            {
+                MouseOperations.SetCursorPosition(x, y);
+            }
+        }
+        public static void MouseEvent(MouseOperations.MouseEventFlags value)
+        {
+            if (adb)
+            {
+                if (value == MouseOperations.MouseEventFlags.LeftUp)
+                {
+                    double scale = 1;
+                    Process.Start(new ProcessStartInfo("adb.exe", $"{adbport}shell input tap {adb_x * scale} {adb_y * scale}"))?.WaitForExit(-1);
+                }
+            }
+            else
+            {
+                MouseOperations.MouseEvent(value);
+            }
         }
     }
 }
--- autohelper/Program.cs
+++ autohelper/Program.cs
@@ -6,6 +6,7 @@ using System.Diagnostics;
 using System.DirectoryServices.ActiveDirectory;
 using System.Text;
 using Microsoft.CSharp;
+using OpenCvSharp.XFeatures2D;
 
 
 // todo gpu acc, location game window, call return
@@ -80,6 +81,14 @@ namespace autohelper
             });
             return desc;
         }
+
+        public class ImgData
+        {
+            public Mat src;
+            public Mat desc = new Mat();
+            public KeyPoint[] KeyPoints;
+        }
+
         public static int ToInt(string str)
         {
             int num = 0;
@@ -96,13 +105,13 @@ namespace autohelper
             return flag ? -num : num;
         }
     }
-    internal class Program
+    internal partial class Program
     {
-        private static Dictionary<ImgDesc, Mat> clickimg = new Dictionary<ImgDesc, Mat>();
+        private static Dictionary<ImgDesc, ImgDesc.ImgData> clickimg = new Dictionary<ImgDesc, ImgDesc.ImgData>();
         private static StringBuilder records = new StringBuilder();
-        private static Dictionary<ImgDesc, Mat> defaultimg = new Dictionary<ImgDesc, Mat>();
+        private static Dictionary<ImgDesc, ImgDesc.ImgData> defaultimg = new Dictionary<ImgDesc, ImgDesc.ImgData>();
         private static Random rand;
-
+        public static SURF sift;
         static void loadimg()
         {
             clickimg.Clear();
@@ -113,12 +122,18 @@ namespace autohelper
             {
                 var sf = Path.GetFileNameWithoutExtension(file);
                 var src = Cv2.ImRead(file);
+                ImgDesc.ImgData data = new ImgDesc.ImgData();
+                data.src = src;
+                Mat gray = new Mat();
+                Cv2.CvtColor(src, gray, ColorConversionCodes.BGR2GRAY);
+
+                sift.DetectAndCompute(gray, null, out data.KeyPoints, data.desc);
                 src.ConvertTo(src, MatType.CV_32FC3);
 
                 var desc = ImgDesc.Parse(sd, sf);
-                clickimg.Add(desc, src);
+                clickimg.Add(desc, data);
                 if (desc.isDefault)
-                    defaultimg.Add(desc, src);
+                    defaultimg.Add(desc, data);
             };
 
             Action<string> loaddir = (s) =>
@@ -160,8 +175,13 @@ namespace autohelper
                 Mat src = makeScreenshot();
                 
                 Cv2.CvtColor(src, src, ColorConversionCodes.BGRA2BGR, 3);
+                ImgDesc.ImgData data = new ImgDesc.ImgData();
+                data.src = src;
+                Mat gray = new Mat();
+                Cv2.CvtColor(src, gray, ColorConversionCodes.BGR2GRAY);
                 src.ConvertTo(src, MatType.CV_32FC3);
 
+                sift.DetectAndCompute(gray, null, out data.KeyPoints, data.desc);
                 Console.WriteLine(lines[i]);
 
                 ImgDesc lastfd = null;
@@ -186,7 +206,7 @@ namespace autohelper
                     if (em.Current.Key.name == lines[i])
                     {
                         double val = em.Current.Key.ide;
-                        var r = search(out rect, src, em.Current.Value);
+                        var r = search(out rect, data, em.Current.Value);
                         Console.WriteLine($"{em.Current.Key.name}: {r} {r >= val}");
                         if (r > em.Current.Key.ide)
                         {
@@ -209,7 +229,7 @@ namespace autohelper
                             if (em.Current.Key.name == lastfd.selectImage && em.Current.Key.dir == lastfd.dir)
                             {
                                 double val = em.Current.Key.ide;
-                                var r = search(out rect, src, em.Current.Value);
+                                var r = search(out rect, data, em.Current.Value);
                                 Console.WriteLine($"{em.Current.Key.name}: {r} {r >= val}");
                                 if (r > em.Current.Key.ide)
                                 {
@@ -239,6 +259,13 @@ namespace autohelper
                 Mat src = makeScreenshot();
                 //src.CvtColor(ColorConversionCodes.RGBA2RGB, 3);
                 Cv2.CvtColor(src, src, ColorConversionCodes.BGRA2BGR, 3);
+
+                ImgDesc.ImgData data = new ImgDesc.ImgData();
+                data.src = src;
+
+                Mat gray = new Mat();
+                Cv2.CvtColor(src, gray, ColorConversionCodes.BGR2GRAY);
+                sift.DetectAndCompute(gray, null, out data.KeyPoints, data.desc);
                 src.ConvertTo(src, MatType.CV_32FC3);
 
 
@@ -280,7 +307,7 @@ namespace autohelper
                         if (em.Current.Key.name == lastfd.name && em.Current.Key.dir == lastfd.dir)
                         {
                             double val = em.Current.Key.ide;
-                            var r = search(out rect, src, em.Current.Value);
+                            var r = search(out rect, data, em.Current.Value);
                             Console.WriteLine($"{em.Current.Key.name}: {r} {r >= val}");
                             if (r > em.Current.Key.ide)
                             {
@@ -292,7 +319,7 @@ namespace autohelper
                         }
                     }
                 }
-                ImgDesc fd = search(ref rect, src);
+                ImgDesc fd = search(ref rect, data);
 
                 if (fd != null)
                 {
@@ -311,7 +338,7 @@ namespace autohelper
                             if (em.Current.Key.name == fd.selectImage && em.Current.Key.dir == fd.dir)
                             {
                                 double val = em.Current.Key.ide;
-                                var r = search(out rect, src, em.Current.Value);
+                                var r = search(out rect, data, em.Current.Value);
                                 Console.WriteLine($"{em.Current.Key.name}: {r} {r >= val}");
                                 if (r > em.Current.Key.ide)
                                 {
@@ -330,7 +357,7 @@ namespace autohelper
 
                     var desc = list[idx];
                     double val = desc.Key.ide;
-                    var r = search(out rect, src, desc.Value);
+                    var r = search(out rect, data, desc.Value);
                     Console.WriteLine($"default {desc.Key.name}: {r} {r >= val}");
                     if (r > desc.Key.ide)
                     {
@@ -347,7 +374,7 @@ namespace autohelper
             }
         }
 
-        static ImgDesc search(ref Rect rect, Mat src)
+        static ImgDesc search(ref Rect rect, ImgDesc.ImgData src)
         {
             List<KeyValuePair<ImgDesc, Rect>> finds = new List<KeyValuePair<ImgDesc, Rect>>();
             var em = clickimg.GetEnumerator();
@@ -381,23 +408,59 @@ namespace autohelper
             return null;
         }
 
-        static double search(out Rect rect, Mat src, Mat tosearch)
+        private static bool useSurf = true;
+        static double search(out Rect rect, ImgDesc.ImgData src, ImgDesc.ImgData tosearch)
         {
-            Mat out1 = new Mat();
-            Cv2.MatchTemplate(src, tosearch, out1, TemplateMatchModes.CCoeffNormed);
-
-            
-            double r1, r2;
-            Point minLoc, maxLoc;
-            out1.MinMaxLoc(out r1, out r2, out minLoc, out maxLoc);
+            if (!useSurf)
+            {
+                Mat out1 = new Mat();
+                Cv2.MatchTemplate(src.src, tosearch.src, out1, TemplateMatchModes.CCoeffNormed);
 
 
-            rect.X = maxLoc.X;
-            rect.Y = maxLoc.Y;
-            rect.Width = tosearch.Width;
-            rect.Height = tosearch.Height;
+                double r1, r2;
+                Point minLoc, maxLoc;
+                out1.MinMaxLoc(out r1, out r2, out minLoc, out maxLoc);
 
-            return r2;
+                rect.X = maxLoc.X;
+                rect.Y = maxLoc.Y;
+                rect.Width = tosearch.src.Width;
+                rect.Height = tosearch.src.Height;
+
+                return r2;
+            }
+
+            var w = tosearch.src.Width;
+            var h = tosearch.src.Height;
+            Point[] drawingPoints;
+            List<DMatch> goods;
+            double val = GetMatchRect((KeyPoint[])tosearch.KeyPoints.Clone(), (KeyPoint[])src.KeyPoints.Clone(),
+                tosearch.desc.Clone(), src.desc.Clone(), 0.5f, w, h, out drawingPoints, out goods);
+
+            if (drawingPoints != null)
+            {
+                int minx = Math.Min(drawingPoints[0].X, drawingPoints[1].X);
+                int minx1 = Math.Min(drawingPoints[2].X, drawingPoints[3].X);
+
+                int miny = Math.Min(drawingPoints[0].Y, drawingPoints[1].Y);
+                int miny1 = Math.Min(drawingPoints[2].Y, drawingPoints[3].Y);
+
+                int maxx = Math.Max(drawingPoints[0].X, drawingPoints[1].X);
+                int maxx1 = Math.Max(drawingPoints[2].X, drawingPoints[3].X);
+
+                int maxy = Math.Max(drawingPoints[0].Y, drawingPoints[1].Y);
+                int maxy1 = Math.Max(drawingPoints[2].Y, drawingPoints[3].Y);
+
+
+                rect.X = Math.Min(minx, minx1);
+                rect.Y = Math.Min(miny, miny1);
+                rect.Width = Math.Max(maxx, maxx1) - rect.X;
+                rect.Height = Math.Max(maxy, maxy1) - rect.Y;
+            }
+            else
+            {
+                rect = default(Rect);
+            }
+            return 1 - val;
         }
 
         public static void parseKey(string arg, Action<string, string> call)
@@ -417,7 +480,100 @@ namespace autohelper
         private static bool record = false;
         private static bool play = false;
 
-        private static string imgdir = "";
+        static double GetMatchRect(KeyPoint[] keypoints1, KeyPoint[] keypoints2,
+            Mat descriptors1, Mat descriptors2, float ide, int w, int h, out Point[] drawingPoints, out List<DMatch> goods)
+        {
+            if (keypoints1.Length == 0 || keypoints2.Length == 0)
+            {
+                goods = null;
+                drawingPoints = null;
+                return 999;
+            }
+            // Match descriptor vectors
+            using var flannMatcher = new BFMatcher();
+            Console.WriteLine($"{descriptors1.Type()} {keypoints1.Length} {descriptors2.Type()} {keypoints2.Length}");
+            DMatch[] flannMatches = flannMatcher.Match(descriptors1, descriptors2);
+            List<DMatch> list = flannMatches.ToList();
+            list.Sort((a, b) =>
+            {
+                return a.Distance.CompareTo(b.Distance);
+            });
+            float mindis = list[0].Distance;
+            goods = new List<DMatch>();
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i].Distance < MathF.Max(mindis * 2, ide) && goods.Count < 10)
+                {
+                    goods.Add(list[i]);
+                }
+                //Console.WriteLine($"{list[i]}");
+            }
+
+            if (goods.Count > 4)
+            {
+                List<Point2f> p1 = new List<Point2f>();
+                List<Point2f> p2 = new List<Point2f>();
+                for (int i = 0; i < goods.Count; i++)
+                {
+                    p1.Add(keypoints1[goods[i].QueryIdx].Pt);
+                    p2.Add(keypoints2[goods[i].TrainIdx].Pt);
+                }
+                var mat = Cv2.FindHomography(InputArray.Create(p1), InputArray.Create(p2), HomographyMethods.USAC_DEFAULT);
+                List<Point2f> p0 = new List<Point2f>();
+
+                p0.Add(new Point2f(0, 0));
+                p0.Add(new Point2f(0, h - 1));
+                p0.Add(new Point2f(w - 1, h - 1));
+                p0.Add(new Point2f(w - 1, 0));
+
+                if (!mat.Empty())
+                {
+                    var p01 = Cv2.PerspectiveTransform(p0, mat);
+                    drawingPoints = p01.Select(p => (Point)p).ToArray();
+                    var orgPoints = p0.Select(p => (Point)p).ToArray();
+
+                    var ms = Cv2.MatchShapes(orgPoints, drawingPoints, ShapeMatchModes.I1);
+                    return ms;
+                }
+            }
+
+            drawingPoints = null;
+            return 999;
+        }
+
+        private static void MatchBySift(Mat src1, Mat src2)
+        {
+            using var gray1 = new Mat();
+            using var gray2 = new Mat();
+            Cv2.CvtColor(src1, gray1, ColorConversionCodes.BGR2GRAY);
+            Cv2.CvtColor(src2, gray2, ColorConversionCodes.BGR2GRAY);
+
+            // Detect the keypoints and generate their descriptors using SIFT
+            using var sift = SURF.Create(300);
+            using Mat<float> descriptors1 = new Mat<float>();
+            using Mat<float> descriptors2 = new Mat<float>();
+            sift.DetectAndCompute(gray1, null, out KeyPoint[] keypoints1, descriptors1);
+            sift.DetectAndCompute(gray2, null, out KeyPoint[] keypoints2, descriptors2);
+
+            var w = gray1.Width;
+            var h = gray1.Height;
+            Point[] drawingPoints;
+            List<DMatch> goods;
+            double val = GetMatchRect(keypoints1, keypoints2, descriptors1, descriptors2, 0.4f, w, h, out drawingPoints, out goods);
+            Console.WriteLine($"MatchShapes {val}");
+
+            if(drawingPoints != null)
+                Cv2.Polylines(gray2, new[] { drawingPoints }, true, Scalar.Aqua, 1);
+
+            // Draw matches
+            using var flannView = new Mat();
+            Cv2.DrawMatches(gray1, keypoints1, gray2, keypoints2, goods.ToArray(), flannView);
+
+            new Window("SIFT matching (by FlannBasedMatcher)", flannView);
+            Cv2.WaitKey();
+        }
+
+        private static string imgdir;
 
         static void Main(string[] args)
         {
@@ -459,6 +615,9 @@ namespace autohelper
                     case "imgdir":
                         imgdir = k;
                         break;
+                    case "useSurf":
+                        useSurf = bool.Parse(k);
+                        break;
                     default:
                         if (!string.IsNullOrEmpty(k))
                             int.TryParse(k, out delay);
@@ -478,11 +637,16 @@ namespace autohelper
                     useKey(m, k);
                 });
             }
+            sift = SURF.Create(300);
             loadimg();
 
             if (tes)
             {
                 test();
+
+                Mat src1 = makeScreenshot();
+                Mat src2 = new Mat("./test.png");
+                MatchBySift(src2, src1);
                 return;
             }
 
@@ -536,98 +700,5 @@ namespace autohelper
 
         }
 
-        public static Mat makeScreenshot()
-        {
-            if (adb)
-                return makeScreenshotAdb();
-            else
-                return BitmapConverter.ToMat(makeScreenshotbmp());
-        }
-
-        public static Bitmap makeScreenshotbmp()
-        {
-            Bitmap screenshot = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height, PixelFormat.Format32bppArgb);
-
-            Graphics gfxScreenshot = Graphics.FromImage(screenshot);
-
-            gfxScreenshot.CopyFromScreen(Screen.PrimaryScreen.Bounds.X, Screen.PrimaryScreen.Bounds.Y, 0, 0, Screen.PrimaryScreen.Bounds.Size, CopyPixelOperation.SourceCopy);
-
-            gfxScreenshot.Dispose();
-
-            return screenshot;
-        }
-
-        public static bool adb = false;
-        private static string adbport = "";
-
-        private static void initAdb(bool _screenshotadb)
-        {
-            adb = true;
-        }
-
-        public static Mat makeScreenshotAdb()
-        {
-            Process pr2 = new Process();
-            pr2.StartInfo.FileName = @"c:\windows\system32\cmd.exe";
-            pr2.StartInfo.Arguments = $"/c \"adb.exe {adbport}exec-out screencap -p > adb.png\"";
-            pr2.Start();
-            pr2.WaitForExit();
-
-            return Cv2.ImRead("adb.png");
-
-            var outputStream = new StreamWriter("adb.png");
-            Process process = new Process();
-            process.StartInfo.FileName = "adb.exe";
-            process.StartInfo.Arguments = "exec-out screencap -p";
-            process.StartInfo.UseShellExecute = false;
-            process.StartInfo.RedirectStandardOutput = true;
-            process.OutputDataReceived += new DataReceivedEventHandler((sender, e) =>
-            {
-                if (!String.IsNullOrEmpty(e.Data))
-                {
-                    outputStream.WriteLine(e.Data);
-                }
-            });
-
-            process.Start();
-
-            process.BeginOutputReadLine();
-
-            process.WaitForExit();
-            process.Close();
-
-            outputStream.Close();
-            
-            return Cv2.ImRead("adb.png");
-        }
-
-        private static int adb_x, adb_y;
-        public static void SetCursorPosition(int x, int y)
-        {
-            if (adb)
-            {
-                adb_x = x;
-                adb_y = y;
-            }
-            else
-            {
-                MouseOperations.SetCursorPosition(x, y);
-            }
-        }
-        public static void MouseEvent(MouseOperations.MouseEventFlags value)
-        {
-            if (adb)
-            {
-                if (value == MouseOperations.MouseEventFlags.LeftUp)
-                {
-                    double scale = 1;
-                    Process.Start(new ProcessStartInfo("adb.exe", $"{adbport}shell input tap {adb_x * scale} {adb_y * scale}"))?.WaitForExit(-1);
-                }
-            }
-            else
-            {
-                MouseOperations.MouseEvent(value);
-            }
-        }
     }
 }
